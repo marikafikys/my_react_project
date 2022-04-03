ab46e2f88d83f1415629a9caa055c4da3b975583
Marina <marikaafikys@gmail.com>
2022-04-03 22:09:51 +0300

    Вторая версия проекта: замена переменных на стейты

--- src/App.jsx
+++ src/App.jsx
@@ -5,55 +5,65 @@ import { Header } from './components/Header';
 import { PostsList } from './components/PostsList';
 import api from './utils/Api';
 
-const elementsOnPage = 9;
-let fullData = [];
-let pageData = [];
+// const elementsOnPage = 9;
+// let fullData = [];
+// let pageData = [];
 let countPosts = 1;
-let currentPostPage = 1;
+// let currentPostPage = 1;
 
 
 export const App = () => {
-	const [posts, setPosts] = useState([]);
+	const [allPosts, setAllPosts] = useState([]);
+	const [postsPerPage, setPostsPerPage] = useState(9);
+	const [pagePosts, setPagePosts] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
 	const [currentUser, setCurrentUser] = useState({});
 	// const [isLoading, setIsLoading] = useState(false); - стейт для спиннера
 
 	useEffect(() => {
 		Promise.all([api.getPostsList(), api.getUserInfo()])
 			.then(([allPostsData, userData]) => {
-				fullData = allPostsData;
-				pageData = allPostsData.slice(0, elementsOnPage);
-				countPosts = fullData.length;
-				//setPosts(pageData);
-				setCurrentUser(userData)
+				setAllPosts(allPostsData)
+				setPagePosts(allPostsData.slice(0, postsPerPage))
+				setCurrentUser(userData);
 			})
 	}, [])
+	console.log("Я делаю рендер");
+	countPosts = allPosts.length;
 
 	const handleClickCreate = () => {
 		console.log('Кнопка нажата');
 	}
 
-	function onChangePage(data, currentPage) {
-		setPosts(data);
-		pageData = data;
-		currentPostPage = currentPage;
+	function onChangePage(newPageData, current, pageSize) {
+		setPagePosts(newPageData);
+		setPostsPerPage(pageSize);
+		setCurrentPage(current);
 	}
 	
 	// обновление постов на текущей странице при удалении поста. Возвращает массив постов для текущей страницы, если это был последний пост на странице возвращает посты с предыдущей страницы
-	function updatePageData(data, currentPage, pageSize)
+	function updatePageDelete(allPosts, currentPage, postsPerPage)
 	{
-		let newData = data.slice(pageSize*(currentPage-1), pageSize*currentPage);
+		let newData = allPosts.slice(postsPerPage*(currentPage-1), postsPerPage*currentPage);
 		let newCurrentPage = newData.length == 0? Math.max(1, currentPage - 1) : currentPage;
 		if (newCurrentPage !== currentPage)
 		{
-			newData = data.slice(pageSize*(newCurrentPage-1), pageSize*newCurrentPage);
+			newData = allPosts.slice(postsPerPage*(newCurrentPage-1), postsPerPage*newCurrentPage);
 		}
 		return newData;
 	}
 	
-	// возвращает индекс текущей страницы при удалении поста
-	function updateCurrentPage(postsCount, currentPage, pageSize)
+	//обновление страницы после лайка
+	function updatePageData(allPosts, currentPage, postsPerPage)
 	{
-		let newCurrentPage = Math.ceil(postsCount/pageSize) < currentPage? Math.max(1, currentPage - 1) : currentPage;
+		let newData = allPosts.slice(postsPerPage*(currentPage-1), postsPerPage*currentPage);
+		return newData;
+	}
+
+	// возвращает индекс текущей страницы при удалении поста
+	function updateCurrentPage(countPosts, currentPage, postsPerPage)
+	{
+		let newCurrentPage = Math.ceil(countPosts/postsPerPage) < currentPage? Math.max(1, currentPage - 1) : currentPage;
 		return newCurrentPage;
 	}
 
@@ -69,12 +79,13 @@ export const App = () => {
 		const isLiked = likes.some(id => id === currentUser._id);
 		api.changeLikeStatus(_id, isLiked)
 			.then((newPost) => {
-				const newPostsState = posts.map(post => {
-					return post._id === newPost._id ? newPost : post
+				const newPostsState = allPosts.map(post => {
+					return post._id === newPost._id ? newPost : post;
 				});
-				setPosts(newPostsState);
-
-			})
+			setAllPosts(newPostsState);
+			setPagePosts(updatePageData(newPostsState, currentPage, postsPerPage));
+			});
+			
 	}
 
 	function handleDeletePost({_id}) {
@@ -84,11 +95,10 @@ export const App = () => {
 				.then(deletedPost => {
 					api.getPostsList()
 						.then(data => {
-							fullData = data;
-							pageData = updatePageData(data, currentPostPage, elementsOnPage);
-							countPosts = fullData.length;
-							currentPostPage = updateCurrentPage(countPosts, currentPostPage, elementsOnPage)
-							setPosts(data);
+							setPagePosts(updatePageDelete(data, currentPage, postsPerPage));
+							countPosts = data.length;
+							setCurrentPage(updateCurrentPage(countPosts, currentPage, postsPerPage));
+							setAllPosts(data);
 						})
 				});
 		}
@@ -117,14 +127,14 @@ export const App = () => {
 				{/* <Spin tip="Loading..."/> */}
 				<PostsList 
 					onChangePage={onChangePage}
-					pageData={pageData}
-					postsData={fullData}  
-					elementsOnPage={elementsOnPage}
+					pagePosts={pagePosts}
+					allPosts={allPosts}  
+					postsPerPage={postsPerPage}
 					onPostLike={handlePostLike}
 					currentUser={currentUser}
 					handleDeletePost={handleDeletePost}
-					postsCount={countPosts}
-					currentPage = {currentPostPage} // отправляю индекс текущей страницы для корректной работы пагинации при удалении постов
+					countPosts={countPosts}
+					currentPage = {currentPage} // отправляю индекс текущей страницы для корректной работы пагинации при удалении постов
 					/>
 			</div>
 			
--- src/components/PostsList/index.jsx
+++ src/components/PostsList/index.jsx
@@ -5,21 +5,22 @@ import { Pagination } from "antd";
 // import { postData } from '../../posts';
 
 
-export const PostsList = ({onChangePage, postsData, pageData, elementsOnPage, onPostLike, currentUser, handleDeletePost, postsCount, currentPage}) => {
-	console.log(currentPage);
-	const handlePageChange = (defaultCurrent, pageSize) => {
-		let newPageData = postsData.slice(pageSize*(defaultCurrent-1), pageSize*defaultCurrent);
-		onChangePage(newPageData, defaultCurrent);
+export const PostsList = ({pagePosts, allPosts, postsPerPage, onPostLike, currentUser, countPosts, currentPage, onChangePage, handleDeletePost}) => {
+	const handlePageChange = (current, pageSize) => {
+		let newPageData = allPosts.slice(pageSize*(current-1), pageSize*current);
+		onChangePage(newPageData, current, pageSize);
 	}
 
 	return (
 		<>
 		<div className="postsList">
-			{pageData.map(el => <Post key={el._id} {...el} onPostLike={onPostLike} onDeletePost={handleDeletePost} currentUser={currentUser}/>)}
+			{pagePosts.map(el => <Post key={el._id} {...el} onPostLike={onPostLike} 
+			onDeletePost={handleDeletePost} 
+			currentUser={currentUser}/>)}
 		</div>
 		<div className="pagination">
 			
-			<Pagination onChange={handlePageChange} pageSize={elementsOnPage} total={postsCount} current = {currentPage}/>
+			<Pagination onChange={handlePageChange} pageSize={postsPerPage} pageSizeOptions={[3,6,9,12]} total={countPosts} current={currentPage}/>
 		</div>
 		</>
 	)
